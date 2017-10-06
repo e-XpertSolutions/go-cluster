6a64cd081e909a99d0f4f28b9056e835524cde12
Marta Kuziora <marta.kuziora@e-xpertsolutions.com>
2017-10-06 15:34:44 +0200

    cluster: add comments

--- cluster/matrix.go
+++ cluster/matrix.go
@@ -13,14 +13,17 @@ type DenseVector struct {
 	*mat.VecDense
 }
 
+// NewDenseVector creates new DenseVector
 func NewDenseVector(n int, data []float64) *DenseVector {
 	return &DenseVector{VecDense: mat.NewVecDense(n, data)}
 }
 
+// DenseMatrix wraps *gonum.org/v1/gonum/mat.Dense type.
 type DenseMatrix struct {
 	*mat.Dense
 }
 
+//NewDenseMatrix creates new DenseMatrix
 func NewDenseMatrix(r, c int, data []float64) *DenseMatrix {
 	return &DenseMatrix{Dense: mat.NewDense(r, c, data)}
 }
